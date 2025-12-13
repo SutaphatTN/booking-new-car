8cbec8d265bb7d4ed271ea0e5589e6ad09cadd03
SutaphatTN <sutaphat.thongnui@gmail.com>
2025-12-13 10:36:44 +0700

    fix : file extension

--- resources/views/layouts/sections/scriptsIncludes.blade.php
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Vite;
 @endphp
 
-@vite(['resources/css/app.scss', 'resources/js/app.js'])
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <!-- Place this tag in your head or just before your close body tag. -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
