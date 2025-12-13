06cf7995705aabe9e5e902efd4dfcc8dab634d47
SutaphatTN <sutaphat.thongnui@gmail.com>
2025-12-13 15:49:49 +0700

    update : structure theme

--- resources/views/layouts/sections/scripts.blade.php
+++ resources/views/layouts/sections/scripts.blade.php
@@ -5,12 +5,13 @@
 <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
 <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
 <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/libs/select2/select2.min.js') }}"></script>
 
 <!-- Sneat Main -->
 <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
 <script src="{{ asset('assets/js/main.js') }}"></script>
 
-@stack('pricing-script')
 @yield('page-script')
 
 <!-- Your App JS (Vite only) -->
--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -3,9 +3,22 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700&display=swap" rel="stylesheet">
 
-<!-- Sneat Core CSS -->
+<!-- Sneat Core -->
 <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+
+<!-- Sarabun (override Bootstrap variable) -->
+<link rel="stylesheet" href="{{ asset('assets/css/fonts.css') }}">
+
+<link rel="stylesheet" href="{{ asset('assets/css/inputs.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/tables.css') }}">
+
+<link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables/datatables.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.min.css') }}">
+
+<!-- Sneat Boxicons -->
+<link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons/boxicons.min.css') }}">
+
 
 @yield('vendor-style')
 @yield('page-style')
