bc3f69291c8cc7717ff4d468a361c583015d5cd1
SutaphatTN <sutaphat.thongnui@gmail.com>
2025-12-13 10:34:49 +0700

    update : app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,5 +8,11 @@ import.meta.glob([
   '../assets/vendor/fonts/**'
 ]);
 
+// laravel style -->
+import '../assets/vendor/js/helpers';
+
+// ? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file. 
+import '../assets/js/config';
+
 import Swal from 'sweetalert2';
 window.Swal = Swal;
