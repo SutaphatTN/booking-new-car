7bdd981a62f2ec356b2b5063a92acfd1e4491ea1
SutaphatTN <sutaphat.thongnui@gmail.com>
2025-12-13 10:44:47 +0700

    update : move file js

--- resources/views/layouts/sections/scripts.blade.php
+++ resources/views/layouts/sections/scripts.blade.php
@@ -1,24 +1,6 @@
-@vite([
-'resources/assets/vendor/libs/datatables/datatables.min.css',
-'resources/assets/vendor/libs/select2/select2.min.css',
-])
-
-<!-- BEGIN: Vendor JS-->
-
-@vite([
-'resources/assets/vendor/libs/jquery/jquery.js',
-'resources/assets/vendor/libs/popper/popper.js',
-'resources/assets/vendor/js/bootstrap.js',
-'resources/assets/vendor/libs/datatables/datatables.min.js',
-'resources/assets/vendor/libs/select2/select2.min.js',
-])
-
-@vite(['resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js', 'resources/assets/vendor/js/menu.js'])
-
 @yield('vendor-script')
 <!-- END: Page Vendor JS-->
 <!-- BEGIN: Theme JS-->
-@vite(['resources/assets/js/main.js'])
 
 <!-- END: Theme JS-->
 <!-- Pricing Modal JS-->
